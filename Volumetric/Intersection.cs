8cbcdcd6c2f9c53b898f3199b68c5fb9389b9175
MOISIUC IOANA-CLAUDIA <91494743+ClaudiaIoana@users.noreply.github.com>
2024-02-08 21:13:02 +0200

    Add files via upload

---
+++ Volumetric/Intersection.cs
@@ -0,0 +1,58 @@
+﻿namespace rt
+{
+    public class Intersection
+    {
+        public static readonly Intersection NONE = new();
+
+        public bool Valid { get; set; }
+        public bool Visible { get; set; }
+        public double T { get; }
+        public Vector Position { get; }
+        public Geometry Geometry { get; }
+        public Line Line { get; }
+        public Vector Normal { get; }
+        public Material Material { get; set; }
+        public Color Color { get; set; }
+
+        public Intersection()
+        {
+            Geometry = null;
+            Line = null;
+            Valid = false;
+            Visible = false;
+            T = 0;
+            Position = null;
+            Normal = null;
+            Material = new();
+            Color = new();
+        }
+
+        public Intersection(bool valid, bool visible, Geometry geometry, Line line, double t, Vector normal, Material material, Color color)
+        {
+            Geometry = geometry;
+            Line = line;
+            Valid = valid;
+            Visible = visible;
+            T = t;
+            Normal = normal;
+            Position = Line.CoordinateToPosition(t);
+            Material = material;
+            Color = color;
+        }
+
+        public Intersection(double minT, double maxT, Line line, double t, Vector normal)
+        {
+            Line = line;
+            Valid = true;
+            T = t;
+            Position = Line.CoordinateToPosition(t);
+            Visible = T >= minT && T <= maxT;
+            Normal = normal;
+        }
+
+        public bool IsUseable()
+        {
+            return Valid && Visible;
+        }
+    }
+}
